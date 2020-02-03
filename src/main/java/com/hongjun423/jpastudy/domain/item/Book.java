440958078721725c1b90eb3526db49501a4461c1
hongjun423 <hongjc423@naver.com>
2020-02-03 23:08:14 +0900

    조인전략

--- src/main/java/com/hongjun423/jpastudy/domain/item/Book.java
+++ src/main/java/com/hongjun423/jpastudy/domain/item/Book.java
@@ -1,16 +1,13 @@
 package com.hongjun423.jpastudy.domain.item;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
 
 @Entity
-@Getter
-@Setter
 @DiscriminatorValue("B")
-public class Book extends Item{
+@PrimaryKeyJoinColumn(name = "book_id")
+public class Book extends Item {
 
     private String author;
     private String isbn;
--- src/main/java/com/hongjun423/jpastudy/domain/item/Item.java
+++ src/main/java/com/hongjun423/jpastudy/domain/item/Item.java
@@ -1,17 +1,9 @@
 package com.hongjun423.jpastudy.domain.item;
 
-import com.hongjun423.jpastudy.domain.Category;
-import lombok.Getter;
-import lombok.Setter;
-
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
-@Getter
-@Setter
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Inheritance(strategy = InheritanceType.JOINED)
 @DiscriminatorColumn(name = "dtype")
 public class Item {
 
@@ -23,10 +15,4 @@ public class Item {
     private String name;
 
     private int price;
-
-    private int stockQuantity;
-
-    @ManyToMany(mappedBy = "items")
-    private List<Category> categories = new ArrayList<>();
-
 }
