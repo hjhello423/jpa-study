fc54865f1102bb56ae98ef4a923830a74b49ad56
hongjun423 <hongjc423@naver.com>
2020-02-03 23:14:53 +0900

    구현 클래스마다 테이블 전략

--- src/main/java/com/hongjun423/jpastudy/domain/item/Item.java
+++ src/main/java/com/hongjun423/jpastudy/domain/item/Item.java
@@ -3,7 +3,7 @@ package com.hongjun423.jpastudy.domain.item;
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @DiscriminatorColumn(name = "dtype")
 public class Item {
 
