a831efc4074d16426fab1875071fa62423a5796c
hongjun423 <hongjc423@naver.com>
2020-02-04 22:39:09 +0900

    @Embeddable, @EmbeddedId 사용하여 비식별 관계 매핑

--- src/main/java/com/hongjun423/jpastudy/practice/Parent.java
+++ src/main/java/com/hongjun423/jpastudy/practice/Parent.java
@@ -1,21 +1,13 @@
 package com.hongjun423.jpastudy.practice;
 
-import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.IdClass;
 
 @Entity
-@IdClass(ParentId.class)
 public class Parent {
 
-    @Id
-    @Column(name = "parent_id1")
-    private String id1;
-
-    @Id
-    @Column(name = "parent_id2")
-    private String id2;
+    @EmbeddedId
+    private ParentId id1;
 
     private String name;
 }
--- src/main/java/com/hongjun423/jpastudy/practice/ParentId.java
+++ src/main/java/com/hongjun423/jpastudy/practice/ParentId.java
@@ -1,11 +1,16 @@
 package com.hongjun423.jpastudy.practice;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Embeddable
 public class ParentId implements Serializable {
 
+    @Column(name = "parent_id1")
     private String id1;
+    @Column(name = "parent_id2")
     private String id2;
 
     public ParentId() {
