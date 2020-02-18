961dc3a61789fe5073fdd035950caeb10182a76e
chj <hongjc423@gmail.com>
2020-02-18 21:31:21 +0900

    jpql과 Criteria를 이용한 동적쿼리로 주문 조회
    - jpql을 string으로 동적쿼리 생성하기
    - criteria를 이용해 동적쿼리 생성하기

---
+++ src/main/java/com/hongjun423/jpastudy/repository/OrderSearch.java
@@ -0,0 +1,13 @@
+package com.hongjun423.jpastudy.repository;
+
+import com.hongjun423.jpastudy.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderSearch {
+
+    private String memberName; //회원 이름
+    private OrderStatus orderStatus; //주문 상태 [ORDER, CANCEL]
+}
