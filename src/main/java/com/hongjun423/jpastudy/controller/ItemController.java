b535f42aa7413e24c3028448381f425956f5335a
chj <hongjc423@gmail.com>
2020-02-20 21:56:36 +0900

    상품 목록 조회 추가

--- src/main/java/com/hongjun423/jpastudy/controller/ItemController.java
+++ src/main/java/com/hongjun423/jpastudy/controller/ItemController.java
@@ -1,13 +1,18 @@
 package com.hongjun423.jpastudy.controller;
 
 import com.hongjun423.jpastudy.domain.item.Book;
+import com.hongjun423.jpastudy.domain.item.Item;
 import com.hongjun423.jpastudy.service.ItemService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class ItemController {
@@ -31,5 +36,14 @@ public class ItemController {
 
         itemService.saveItem(book);
         return "redirect:/";
+    }
+
+    @GetMapping("/items")
+    public String list(Model model) {
+        log.debug("come-----------------in");
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
+
     }
 }
